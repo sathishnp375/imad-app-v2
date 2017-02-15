14ea9873928947f5de4771eacb108f813fa4b455
sathishnp375 <sathishnp375@gmail.com>
2017-02-15 12:04:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/student-media 1',function (req, res){
+    res.sendFile(path.join('welcome to student media 1' ));
+});
 
+app.get('/student-media 2',function (req, res){
+    res.sendFile(path.join('welcome to student media 2' ));
+});
+
+app.get('/student-media 3',function (req, res){
+    res.sendFile(path.join('welcome to student media 3' ));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
