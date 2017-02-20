d6690700fc921d999a7e04931eea7cb24e10a46f
sathishnp375 <sathishnp375@gmail.com>
2017-02-20 00:35:18 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 
 var pool = new Pool(config);
 
-app.get('/test-db', function (req, res) {
+//app.get('/test-db', function (req, res) {
     // make a select request
     // make aresponse with the results}
     pool.query('SELECT * FROM test',function(err,result){
