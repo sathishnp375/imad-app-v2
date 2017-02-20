c6728757afa675d5a23ad295a8cce6c8ea0a3b5d
sathishnp375 <sathishnp375@gmail.com>
2017-02-20 00:35:05 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config = {
+  user : 'sathishnp375',
+  database : 'sathishnp375',
+  host :'db.imad.hasura-app.io',
+  port :'5432',
+  password : process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
+
+app.get('/test-db', function (req, res) {
+    // make a select request
+    // make aresponse with the results}
+    pool.query('SELECT * FROM test',function(err,result){
+        
+    });
+
 
 app.get('/studentmedia', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'studentmedia.html'));
