e92d0f433ec65c06db603650f2d5dbf79082065e
sathishnp375 <sathishnp375@gmail.com>
2017-02-21 17:24:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,61 +10,11 @@ var config = {
   password : process.env.DB_PASSWORD
 };
 var app = express();
-//app.use(morgan('combined'));
-
-var studentmedia= {
-    title : 'student media',
-    heading : 'student media',
-    content : `  <div class="container">
-                  <div><pre>                                 <a href='/'>HOME</a>   <a href='https://www.aitsrajampet.ac.in'>MY COLLEGE</a>   <a href='https://onlinecourses.nptel.ac.in/explorer'>NPTEL</a>   <a href=''>MY TUBE</a>   <a href=''>MY MEMORIES</a> </pre>
-              </div>
-        <hr/>
-    <h1 style="text-align:center;"><b>
-         STUDENT MEDIA
-         </b>
-    </h1>
-  <p>  <b>WELCOME to my first WEB APP.</b><br/> My name is sathish . I am the student of Annamacharya Institute Of Technology And Sciences , Rajampet , Kadapa , Andhra  Pradesh. I am the student of  Mechanical Engineering . Now I am second year B.E . In This My STUDENT MEDIA site you can upload your memories of u r collegelike Dance Videos , Images etc.., </p><hr/>
-                     </div>`
-};
-
-function createTemplate (data) {
-    var title= data.title;
-    var heading= data.heading;
-    var content= data.content;
-var htmlTemplate = `<!DOCTYPE html>
-<html>
-    <head>
-        <title>
-                 ${title}
-       </title>
- <link href="/ui/style.css" rel="stylesheet" />
-    
-    </head>
-         <body>
-             
-              <div class="container">
-                  ${content} 
-              </div>
-        <hr/>
-    <h1 style="text-align:center;"><b>
-         STUDENT MEDIA
-         </b>
-    </h1>
-  <p>  <b>WELCOME to my first WEB APP.</b><br/> My name is sathish . I am the student of Annamacharya Institute Of Technology And Sciences , Rajampet , Kadapa , Andhra  Pradesh. I am the student of  Mechanical Engineering . Now I am second year B.E . In This My STUDENT MEDIA site you can upload your memories of u r collegelike Dance Videos , Images etc.., </p><hr/>
-                     </div>
-         </body>
-         </html>
-         
-         return htmlTemplate;
-         }
-
-
-
-
-
+app.use(morgan('combined'));
 app.get('/', function (req, res) {
-  res.send(createTemplate(studentmedia));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 
 app.get('/studentmedia', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'studentmedia.html'));
